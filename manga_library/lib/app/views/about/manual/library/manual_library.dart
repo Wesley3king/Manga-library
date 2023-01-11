b74ffb01a88a36d918af5c7cc86f6c2d5cef7c15
Wesley3king <97459154+Wesley3king@users.noreply.github.com>
2023-01-11 12:25:50 -0300

    Update manual_library.dart

--- manga_library/lib/app/views/about/manual/library/manual_library.dart
+++ manga_library/lib/app/views/about/manual/library/manual_library.dart
@@ -5,26 +5,41 @@ class ManualLibraryPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        Text(
-          "Biblioteca",
-          style: TextStyle(
-            fontSize: 20
-          ),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            const Text(
+              "Biblioteca",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            const Divider(),
+            const Text("A Biblioteca armazena seus livros favoritos, e separa-os por Categorias que você mesmo cria."),
+            const SizedBox(height: 5),
+            Image.asset("assets/manual-imgs/library-3.png", width: 200, height: 380,),
+            const SizedBox(height: 10),
+            const Text("> Criando, editando e deletando uma Categoria",
+              style: TextStyle(
+                fontSize: 17,
+                fontWeight: FontWeight.bold
+              ),),
+            const SizedBox(height: 8),
+            const Text("A edição de Categorias(da biblioteca) localizada em: Outros > Biblioteca."),
+            const Text("Para criar uma Categoria, clique no botão com o icone +, em seguida digite o nome da categoria e clique em Adicionar"),
+            const Text("Você pode alterar a ordem das categorias pressionando seu nome e movendo-o, para salvar a ordem clique no icone de lista."),
+            const SizedBox(height: 5),
+            Image.asset("assets/manual-imgs/library-1.png", width: 200, height: 380,),
+            const SizedBox(height: 10),
+            const Text("Para renomear ou deletar uma Categoria clique no icone de lapis, aparecera uma lista com as Categorias, pressionando no icone lateral de uma categoria aparecera as opções renomear e deletar."),
+            const SizedBox(height: 5),
+            Image.asset("assets/manual-imgs/library-2.png", width: 200, height: 250,),
+          ],
         ),
-        Divider(),
-        Text("A Biblioteca armazena seus livros favoritos, e separa-os por Categorias que você mesmo cria."),
-        Image.asset("assets/manual-imgs/library-3.png"),
-        SizedBox(height: 10),
-        Text("Criando, editando e deletando uma Categoria"),
-        SizedBox(height: 5),
-        Text("A edição de Categorias está em: Outros > Biblioteca."),
-        Text("Para criar uma Categoria, clique no botão com o icone +, em seguida digite o nome da categoria e clique em Adicionar"),
-        Image.asset("assets/manual-imgs/library-1.png"),
-        Text(""),
-        Image.asset("assets/manual-imgs/library-2.png"),
-      ],
+      ),
     );
   }
 }
